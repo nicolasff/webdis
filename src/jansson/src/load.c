2c5403375f74288e16aa02fec38f08338be40b6b
Jessie Murray <jessie.murray@protonmail.com>
2021-04-11 14:53:19 -0700

    Jansson: disable truncation warning locally in error reporting function

--- src/jansson/src/load.c
+++ src/jansson/src/load.c
@@ -59,6 +59,13 @@ typedef struct {
 
 
 /*** error reporting ***/
+/* disable format truncation warnings for this function.
+  it's not a security issue; very long strings could just be truncated on error
+ */
+#if (defined(__GNUC__) && !defined(__clang__))
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-truncation"
+#endif
 
 static void error_set(json_error_t *error, const lex_t *lex,
                       const char *msg, ...)
@@ -101,6 +108,9 @@ static void error_set(json_error_t *error, const lex_t *lex,
 
     jsonp_error_set(error, line, col, "%s", result);
 }
+#if (defined(__GNUC__) && !defined(__clang__))
+#pragma GCC diagnostic pop
+#endif
 
 
 /*** lexical analyzer ***/
